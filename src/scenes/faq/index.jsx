a38f32f9627af348c98b0ec6bd39059c58b58058
Kumar <850066800@genpact.com>
2023-05-19 11:19:50 +0530

    changes to faq

--- src/scenes/faq/index.jsx
+++ src/scenes/faq/index.jsx
@@ -22,60 +22,55 @@ const FAQ = () => {
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
+            Learnzy is an online learning platfoem that offers a wide range of courses taught by experienced tutors. It provides a flexible and interacive learning environment for students of all ages.
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion defaultExpanded>
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
           <Typography color={colors.greenAccent[500]} variant="h5">
-            Important Question
+            Can I interact with my tutors and fellow students?
           </Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
+            Learnzy provides various communication features such as chat and discussion forums. You can interact with your tutor and fellow students, ask questions and participate in group discussions.
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion defaultExpanded>
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
           <Typography color={colors.greenAccent[500]} variant="h5">
-            Your Favorite Question
+            Is my personal information safe on learnzy
           </Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
+            Yes, Learnzy takes the privacy and security of your personal information seriously. We have robust measures in place to protect your data and adhere strict privacy policies.
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion defaultExpanded>
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
           <Typography color={colors.greenAccent[500]} variant="h5">
-            Some Random Question
+            How can I get technical support if I encounter any issues?
           </Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
+            If you encounter any technical issues or need support, you can reach out to our customer support team via email - admin@learnzyAdmin.com. They will be happy to assist your concerns or queries.
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion defaultExpanded>
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
           <Typography color={colors.greenAccent[500]} variant="h5">
-            The Final Question
+            Can I access Learnzy on my mobile device?
           </Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
+            Yes, Learnzy is designed to be mobile-friendly. You can access the platform from which ever platform you like. Tablets, Desktop, Mobile.
           </Typography>
         </AccordionDetails>
       </Accordion>
